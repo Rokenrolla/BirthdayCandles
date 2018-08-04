dda73dd94ebcea136e28e1b0eb226bfc5be0828a
George Smircea <smircea.george@gmail.com>
2018-08-04 21:49:47 +0300

    Add project files.

---
+++ BirthdayCandles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
